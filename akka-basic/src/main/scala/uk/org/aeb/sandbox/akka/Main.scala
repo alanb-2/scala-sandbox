a8ee7c7d59e8b56d8bc5804cefc66126b298499c
Alan Bayley <alan.b2@outlook.com>
2021-12-30 18:29:51 +0000

    feature/basic-akka-app-with-constructor
    Changes
    ----
    * Updated: added 1s pause before stopping actor
    * Updated: added constructor to actor

--- akka-basic/src/main/scala/uk/org/aeb/sandbox/akka/Main.scala
+++ akka-basic/src/main/scala/uk/org/aeb/sandbox/akka/Main.scala
@@ -2,12 +2,12 @@ package uk.org.aeb.sandbox.akka
 
 import akka.actor.{Actor, ActorSystem, Props}
 
-class HelloActor extends Actor {
+class HelloActor(name: String) extends Actor {
 
   def receive = {
 
-    case "hello" => println("Hello, world")
-    case _       => println("?")
+    case "hello" => println(s"$name: Hello, world")
+    case _       => println(s"$name: ?")
 
   }
 
@@ -17,10 +17,12 @@ object Main extends App {
 
   val system = ActorSystem("HelloSystem")
 
-  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  val helloActor = system.actorOf(Props(new HelloActor("Dave")), name = "helloactor")
 
   helloActor ! "hello"
   helloActor ! "hi"
+
+  Thread.sleep(1000)
 
   system.stop(helloActor)
   system.terminate()
